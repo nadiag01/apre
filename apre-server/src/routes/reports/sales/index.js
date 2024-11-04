d2ab1aec935d80b219d1af7c2102c4d41f54aaf3
ky1015 <kylie.struhs@gmail.com>
2024-11-03 18:33:22 -0700

    Added Sales by Category component and tests. Added query to index.js, and tests to index.spec.js.

--- apre-server/src/routes/reports/sales/index.js
+++ apre-server/src/routes/reports/sales/index.js
@@ -78,4 +78,70 @@ router.get('/regions/:region', (req, res, next) => {
   }
 });
 
+/**
+ * @description
+ *
+ * GET /categories
+ *
+ * Fetches a list of distinct sales categories.
+ *
+ * Example:
+ * fetch('/categories')
+ *  .then(response => response.json())
+ *  .then(data => console.log(data));
+ */
+router.get('/categories', (req, res, next) => {
+  try {
+    mongo (async db => {
+      const categories = await db.collection('sales').distinct('category');
+      res.send(categories);
+    }, next);
+  } catch (err) {
+    console.error('Error getting categories: ', err);
+    next(err);
+  }
+});
+
+/**
+ * @description
+ *
+ * GET /categories/:category
+ *
+ * Fetches sales data for a specific category, grouped by salesperson.
+ *
+ * Example:
+ * fetch('/categories/accessories')
+ *  .then(response => response.json())
+ *  .then(data => console.log(data));
+ */
+router.get('/categories/:category', (req, res, next) => {
+  try {
+    mongo (async db => {
+      const salesReportByCategory = await db.collection('sales').aggregate([
+        { $match: { category: req.params.category } },
+        {
+          $group: {
+            _id: '$category',
+            totalSales: { $sum: '$amount'}
+          }
+        },
+        {
+          $project: {
+            _id: 0,
+            category: '$_id',
+            totalSales: 1,
+          }
+        },
+        {
+          $sort: { totalSales: 1 }
+        }
+      ]).toArray();
+      res.send(salesReportByCategory);
+    }, next);
+  } catch (err) {
+    console.error('Error getting sales data for category: ', err);
+    next(err);
+  }
+});
+
 module.exports = router;
